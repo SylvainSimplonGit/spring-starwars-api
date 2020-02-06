7c257983220a83639e92f7a7aab0fd03cd2ae409
SylvainSimplonDev <sylvain.grippon.devweb@gmail.com>
2020-02-06 10:13:57 +0100

    Add integration test getPlanetsList and getPlanet

--- src/test/java/co/simplon/starwarsapi/StarWarsApiApplicationTests.java
+++ src/test/java/co/simplon/starwarsapi/StarWarsApiApplicationTests.java
@@ -2,14 +2,19 @@ package co.simplon.starwarsapi;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.net.http.HttpResponse;
 import java.util.List;
 
+import co.simplon.starwarsapi.model.planet.Planet;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
@@ -20,12 +25,56 @@ class StarWarsApiApplicationTests {
     private TestRestTemplate restTemplate;
 
     @Test
-    public void getPlanets() {
-		// When retrieving planets from /api/planets
-		List<?> planets = this.restTemplate.getForObject("/api/planets", List.class);
-
-		// Then a non null list should be returned
+//    Get planet list
+//    Test de la requête GET sur /apî/planets
+    public void getPlanetsListShouldReturnNotNullList() {
+		// When getting on /api/planets
+//		List<?> planets = this.restTemplate.getForObject("/api/planets", List.class);
+        ResponseEntity<List> responseEntity = this.restTemplate.exchange("/api/planets", HttpMethod.GET, null, List.class);
+        List<?> planets = responseEntity.getBody();
+        // Then a non null list should be returned
 		assertThat(planets).isNotNull();
     }
+
+    @Test
+//    Get existing planet
+//    Test de la requête GET sur /apî/planets/{planetId} sur un ID de planete présent
+    public void getExistingPlanetShouldReturnPlanet() {
+        // When getting on /api/planets/{planetId}
+        ResponseEntity<Planet> responseEntity = this.restTemplate.exchange("/api/planets/1", HttpMethod.GET, null, Planet.class);
+        Planet planet = responseEntity.getBody();
+        // Then code 200 should returned
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        // Then a planet not null should returned
+        assertThat(planet).isNotNull();
+    }
+
+
+    @Test
+//    Get non existing planet
+//    Test de la requête GET sur /apî/planets/{planetId} sur un ID de planete absente
+    public void getNotExistingPlanetShouldReturnPlanet() {
+        // When getting on /api/planets/{planetId}
+        ResponseEntity<Planet> responseEntity = this.restTemplate.exchange("/api/planets/99999999", HttpMethod.GET, null, Planet.class);
+        // Then code 200 should returned
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+    }
+
+//    @Test
+//    Create planet
+//    Test de la requête POST sur /api/planets
+
+//    @Test
+//    Update existing planet
+//    Test de la requête PUT sur /api/planets/{planetId} sur un ID de planete présent
+
+//    @Test
+//    Update non existing planet
+//    Test de la requête PUT sur /api/planets/{planetId} sur un ID de planete absente
+
+//    @Test
+//    Delete  planet
+//    Test de la requête DELETE sur /api/planets/{planetId}
+
 
 }
